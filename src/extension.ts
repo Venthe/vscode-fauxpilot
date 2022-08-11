5982485d9af14af0fb23d3fefc354f90d2453eef
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2022-08-11 21:26:57 +0200

    Refactor extension code

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { Configuration, CreateCompletionResponse, OpenAIApi } from 'openai';
-import { CancellationToken, commands, ExtensionContext, InlineCompletionContext, InlineCompletionItem, InlineCompletionItemProvider, InlineCompletionList, languages, Position, ProviderResult, Range, TextDocument, window, workspace } from 'vscode';
+import { Configuration, CreateCompletionRequestPrompt, CreateCompletionResponse, OpenAIApi } from 'openai';
+import { CancellationToken, ExtensionContext, InlineCompletionContext, InlineCompletionItem, InlineCompletionItemProvider, InlineCompletionList, languages, Position, ProviderResult, Range, TextDocument, window, workspace } from 'vscode';
+import { AxiosResponse } from 'axios';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(_context: ExtensionContext) {
+	function getPrompt(document: TextDocument, position: Position): String | undefined {
+		return document.getText(
+			new Range(position.with(undefined, 0), position)
+		);
+	}
+
+	function isNil(value: String | undefined | null): boolean {
+		return value == undefined || value == null || value.length == 0;
+	}
+
+	function callOpenAi(prompt: String): Promise<AxiosResponse<CreateCompletionResponse, any>> {
+		console.debug("Calling OpenAi", prompt);
+		return openai.createCompletion({
+			model: "fastertransformer",
+			prompt: prompt as CreateCompletionRequestPrompt,
+			max_tokens: workspace.getConfiguration('fauxpilot').get("maxTokens"),
+			temperature: 0.1
+		});
+	}
+
+	function toInlineCompletions(value: CreateCompletionResponse): InlineCompletionItem[] {
+		return value.choices?.map(choice => choice.text).map(choiceText => ({ insertText: choiceText } as InlineCompletionItem)) || [];
+	}
+
 	const configuration = new Configuration({
 		apiKey: "dummy"
 	});
@@ -15,28 +40,30 @@ export function activate(_context: ExtensionContext) {
 		//@ts-ignore
 		// becasue ASYNC and PROMISE
 		provideInlineCompletionItems: async (document: TextDocument, position: Position, _context: InlineCompletionContext, _token: CancellationToken): Promise<ProviderResult<InlineCompletionList | InlineCompletionItem[]>> => {
-			const textBeforeCursor = document.getText(
-				new Range(position.with(undefined, 0), position)
-			);
+			const prompt = getPrompt(document, position);
+			console.debug("Requesting completion for prompt", prompt);
 
-			try {
-				const result = await openai.createCompletion({
-					model: "fastertransformer",
-					prompt: textBeforeCursor,
-					max_tokens: workspace.getConfiguration('fauxpilot').get("maxTokens"),
-					temperature: 0.1
-				});
-				return result.data.choices?.map(a => a.text).map(a => ({ insertText: a } as InlineCompletionItem)) || [];
-			} catch (err: any) {
-				window.showErrorMessage(err);
+			if (isNil(prompt)) {
+				console.debug("Prompt is empty, skipping");
+				return [];
 			}
+
+			// Prompt is already nil-checked
+			const response = await callOpenAi(prompt as String);
+			console.debug("Got response from OpenAi", response);
+			const completions = toInlineCompletions(response.data);
+			console.debug("Transformed completions", completions);
+			return completions;
 		}
 	};
 
+	console.debug("Registering Fauxpilot provider", new Date());
 	languages.registerInlineCompletionItemProvider(
 		{ pattern: "**" }, provider
 	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	console.debug("Deactivating Fauxpilot provider", new Date());
+}
