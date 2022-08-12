f8ed665d7bacd13161c5d4464a9f42cd9d1dfc43
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2022-08-12 10:53:13 +0200

    #8 Fix regression disallowing extension to work on saved files

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: ExtensionContext) {
 	console.debug("Registering Fauxpilot provider", new Date());
 	context.subscriptions.push(
 		languages.registerInlineCompletionItemProvider(
-			{ pattern: "**", scheme: 'untitled' }, new FauxpilotCompletionProvider()
+			{ pattern: "**" }, new FauxpilotCompletionProvider()
 		),
 		commands.registerCommand(turnOnFauxpilot.command, turnOnFauxpilot.callback),
 		commands.registerCommand(turnOffFauxpilot.command, turnOffFauxpilot.callback)
